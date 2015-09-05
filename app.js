0cf1e90cf54f725da12d8907abcbbf33a7e5346c
bluesun23 <sunnybenari@gmail.com>
2015-09-05 16:18:48 +0300

    Add google predication  on /search - NOT FINISH
    when client go to /search/[query] the server return the google
    prediction result for the query.
    NOTICE: the filter 'getStreetFromTlv' not working

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var fileParser = require('./Parser.js');
+var http = require('https');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var googleAPI = 'AIzaSyAFIeUM9E0jdkLT7aNsizf_Iove6TvCj6Y';
 
 
 var routes = require('./routes/index');
@@ -33,6 +36,60 @@ app.use('/users', users);
 
 app.get('/streets/:name', function(request, response) {
   response.send(fileParser.getStreets(request.params.name));
+});
+
+app.get('/search/:search',function(request,response){
+
+  var url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?' +
+      // the street name you want to search in google places,
+      // streetname must be sent as UTF8 in order google servie acccept the request
+      'input=' + encodeURIComponent(request.params.search)+
+      // types - only address result from israel
+      '&types=address&language=il' +
+      '&key='+googleAPI;
+
+  http.get(url, function(res){
+    var body = '';
+
+    res.on('data', function(chunk){
+      body += chunk;
+    });
+
+    res.on('end', function(){
+      var predictionResults = JSON.parse(body);
+
+      // TODO handel status: OK, zero_results, ....
+      var filteredPredictionResults = getStreetFromTlv(predictionResults);
+
+      response.send(filteredPredictionResults);
+      console.log("Got a response: ", body);
+    });
+  }).on('error', function(e){
+    console.log("Got an error: ", e);
+  });
+
+
+  function getStreetFromTlv(data){
+    var filtered;
+
+    data.predictions.forEach(function (result){
+      // if the city is tlv aviv
+
+      console.log('value:#' + result.terms[1].value + '#');
+      console.log('tosrtinrg:#' + result.terms[1].value+ '#');
+      console.log('compare:' + result.terms[1].value === 'תל אביב יפו');
+
+      //TODO: the hewbrew hard coded string making the comperasion to fail
+      if (result.terms[1].value == new String("תל אביב יפו").valueOf()) {
+        // push the street name
+        filtered.push(result.terms[0]);
+      }
+    });
+
+    return filtered;
+  }
+
+
 });
 
 // catch 404 and forward to error handler
